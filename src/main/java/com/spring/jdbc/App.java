7de96a1c5be4de5369e57be41f0363c36abe16b5
Lohat <klohat@deloitte.com>
2023-09-08 12:25:05 +0530

    Initial commit

---
+++ src/main/java/com/spring/jdbc/App.java
@@ -0,0 +1,19 @@
+package com.spring.jdbc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		JdbcTemplate jdbcTemplateBean = context.getBean(JdbcTemplate.class);
+		String insertQuery = "INSERT INTO trainee (id, name, location) VALUES (?, ?, ?)";
+		int result = jdbcTemplateBean.update(insertQuery, 101, "Konark Lohat", "Gachibowli, Hyderabad");
+
+		System.out.println(result);
+
+		((AbstractApplicationContext) context).close();
+	}
+}
